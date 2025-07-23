1385330b459a9e2bea3c33d06620bb5de77d55a1
Nepi <lorenzo.nepitella05@gmail.com>
2025-07-23 14:27:34 +0200

    Aggiunta definizioni distruttore getListSize addActivity e removeActivity

--- Register.cpp
+++ Register.cpp
@@ -1,5 +1,20 @@
-//
-// Created by admin on 23/07/2025.
-//
-
 #include "Register.h"
+
+int Register::getListSize() {
+    return activities.size();
+}
+
+Register::~Register() {
+    for (Activity *activity: activities) {
+        removeActivity(activity);
+    }
+}
+
+void Register::addActivity(Activity *activity) {
+    activities.push_back(activity);
+}
+
+void Register::removeActivity(Activity *activity) {
+    activities.remove(activity);
+}
+
