d494ba800bca65282db931804a00bd2d52b198f3
Nepi <lorenzo.nepitella05@gmail.com>
2025-07-23 13:48:20 +0200

    Definizione costruttore e metodi getter

--- Activity.cpp
+++ Activity.cpp
@@ -1,5 +1,19 @@
-//
-// Created by admin on 23/07/2025.
-//
-
 #include "Activity.h"
+#include <string>
+#include <utility>
+using namespace std;
+
+Activity::Activity(string desc, string start, string end): desc(move(desc)), start(move(start)), end(move(end)) {
+}
+
+string Activity::getDesc() {
+    return desc;
+}
+
+string Activity::getStart() {
+    return start;
+}
+
+string Activity::getEnd() {
+    return end;
+}
